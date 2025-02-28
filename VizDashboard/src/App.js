56cf3284ca24db0ce61a12258c72f5fa0f5d6a2b
Astrasv <116169969+Astrasv@users.noreply.github.com>
2025-02-28 10:30:09 +0530

    Removed Unwanted Folders

--- VizDashboard/src/App.js
+++ VizDashboard/src/App.js
@@ -5,7 +5,7 @@ import { Bar, Doughnut, Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, PointElement, LineElement, ArcElement, Title, Tooltip, Legend } from 'chart.js';
 import './App.css';
 
-// Register Chart.js components
+
 ChartJS.register(CategoryScale, LinearScale, BarElement, PointElement, LineElement, ArcElement, Title, Tooltip, Legend);
 
 function SearchByKeyword() {
